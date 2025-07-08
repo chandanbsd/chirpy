9d903c3c8fb4e627afc2e920affe239505453677
Bangalore Shankar Denanath, Chandan <49138440+chandanbsd@users.noreply.github.com>
2025-07-07 22:00:15 -0400

    Added health check endpoint

--- main.go
+++ main.go
@@ -6,10 +6,19 @@ import (
 	"os"
 )
 
+func healthzHandler(resWriter http.ResponseWriter, req *http.Request) {
+	header := resWriter.Header()
+	header.Set("Content-Type", "text/plain; charset=utf-8")
+	resWriter.WriteHeader(200)
+	resWriter.Write([]byte("OK"))
+}
+
 func main() {
 	serveMux := http.NewServeMux()
 
-	serveMux.Handle("/", http.FileServer(http.Dir(".")))
+	serveMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+
+	serveMux.HandleFunc("/healthz", healthzHandler)
 
 	server := http.Server {
 		Handler: serveMux,

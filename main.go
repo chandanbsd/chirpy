e47f03aecf894ddf0894cd63bfdb6c3172d1fda6
Bangalore Shankar Denanath, Chandan <49138440+chandanbsd@users.noreply.github.com>
2025-07-07 20:27:57 -0400

    Added file server

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
 func main() {
 	serveMux := http.NewServeMux()
 
+	serveMux.Handle("/", http.FileServer(http.Dir(".")))
+
 	server := http.Server {
 		Handler: serveMux,
 		Addr: ":8080",
@@ -16,7 +18,7 @@ func main() {
 
 	err := server.ListenAndServe()
 	if err != nil {
-		fmt.Println("Failed to start server")
+		fmt.Printf("Failed to start server: %v\n", err)
 		os.Exit(1)
 	}
 }

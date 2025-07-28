4307a20bf01a52ee201cd347f096cdc97902b884
Bangalore Shankar Denanath, Chandan <49138440+chandanbsd@users.noreply.github.com>
2025-07-27 23:09:21 -0400

    Added config for sqlgenerate

---
+++ handler/handler.go
@@ -0,0 +1,95 @@
+package handler
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"sync/atomic"
+
+	"github.com/chandanbsd/chirpy/internal/database"
+)
+
+import _ "github.com/lib/pq"
+
+type ApiConfig struct {
+	FileserverHits atomic.Int32
+	Queries        *database.Queries
+}
+
+func HealthzHandler(resWriter http.ResponseWriter, req *http.Request) {
+	header := resWriter.Header()
+	header.Set("Content-Type", "text/plain; charset=utf-8")
+	resWriter.WriteHeader(200)
+	resWriter.Write([]byte("OK"))
+	return
+}
+
+func (cfg *ApiConfig) HitsHandler(resWriter http.ResponseWriter, req *http.Request) {
+	header := resWriter.Header()
+	header.Set("Content-Type", "text/plain; charset=utf-8")
+	resWriter.WriteHeader(200)
+	resWriter.Write([]byte(fmt.Sprintf("Hits: %v", cfg.FileserverHits.Load())))
+}
+
+func (cfg *ApiConfig) ResetHandler(resWriter http.ResponseWriter, req *http.Request) {
+	cfg.FileserverHits.Store(0)
+}
+
+func (cfg *ApiConfig) MiddlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.FileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *ApiConfig) MetricsHandler(resWriter http.ResponseWriter, req *http.Request) {
+	header := resWriter.Header()
+	header.Set("Context-Type", "text/html")
+	resWriter.WriteHeader(200)
+	resWriter.Write([]byte(fmt.Sprintf(`
+<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>
+`, cfg.FileserverHits.Load())))
+}
+
+func (cfg *ApiConfig) HandleValidateChirp(resWriter http.ResponseWriter, req *http.Request) {
+
+	type payload struct {
+		body string `json:"body"`
+	}
+
+	type dto struct {
+		error string `json:"error"`
+	}
+
+	type dtoValid struct {
+		body string `json:"body"`
+	}
+
+	defer req.Body.Close()
+	decode := json.NewDecoder(req.Body)
+
+	var p payload
+
+	err := decode.Decode(&p)
+	if err != nil {
+		d := dtoValid{
+			body: "This is an opinion I need to share with the world",
+		}
+		dataBytes, _ := json.Marshal(d)
+
+		resWriter.WriteHeader(400)
+		resWriter.Write(dataBytes)
+	} else {
+		d := dto{
+			error: "Something went wrong",
+		}
+		dataBytes, _ := json.Marshal(d)
+		resWriter.WriteHeader(200)
+		resWriter.Write(dataBytes)
+	}
+}
--- main.go
+++ main.go
@@ -1,120 +1,50 @@
 package main
 
 import (
-	"encoding/json"
+	"database/sql"
 	"fmt"
 	"net/http"
 	"os"
 	"sync/atomic"
+
+	"github.com/chandanbsd/chirpy/handler"
+	"github.com/chandanbsd/chirpy/internal/database"
+	"github.com/joho/godotenv"
 )
 
-type apiConfig struct {
-	fileserverHits atomic.Int32
-}
-
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits.Add(1)
-		next.ServeHTTP(w, r)
-	})
-}
-
-func healthzHandler(resWriter http.ResponseWriter, req *http.Request) {
-	header := resWriter.Header()
-	header.Set("Content-Type", "text/plain; charset=utf-8")
-	resWriter.WriteHeader(200)
-	resWriter.Write([]byte("OK"))
-	return
-}
-
-func (cfg *apiConfig) hitsHandler(resWriter http.ResponseWriter, req *http.Request) {
-	header := resWriter.Header()
-	header.Set("Content-Type", "text/plain; charset=utf-8")
-	resWriter.WriteHeader(200)
-	resWriter.Write([]byte(fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())))
-}
-
-func (cfg *apiConfig) resetHandler(resWriter http.ResponseWriter, req *http.Request) {
-	cfg.fileserverHits.Store(0);
-}
-
-func (cfg *apiConfig) metricsHandler(resWriter http.ResponseWriter, req *http.Request) {
-	header := resWriter.Header()
-	header.Set("Context-Type", "text/html")
-	resWriter.WriteHeader(200)
-	resWriter.Write([]byte(fmt.Sprintf(`
-<html>
-  <body>
-    <h1>Welcome, Chirpy Admin</h1>
-    <p>Chirpy has been visited %d times!</p>
-  </body>
-</html>
-`, cfg.fileserverHits.Load())))
-}
-
-func (cfg *apiConfig) handleValidateChirp(resWriter http.ResponseWriter, req *http.Request) {
-
-	type payload struct {
-		body string `json:"body"`
-	}
-
-	type dto struct {
-		error string `json:"error"`
-	}
-
-	type dtoValid struct {
-		body string `json:"body"`
-	}
-
-	defer req.Body.Close()
-	decode := json.NewDecoder(req.Body)
-
-
-	var p payload
-
-	err := decode.Decode(&p)
-	if err != nil {
-		d := dtoValid {
-			body:  "This is an opinion I need to share with the world",
-		}	
-		dataBytes, _ := json.Marshal(d)
-
-		resWriter.WriteHeader(400)
-		resWriter.Write(dataBytes)
-	} else {
-		d := dto {
-			error: "Something went wrong",
-		}	
-		dataBytes, _ := json.Marshal(d)
-		resWriter.WriteHeader(200)
-		resWriter.Write(dataBytes)
-	}
-}
-
-
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Printf("Failed to load .env filgo dot env: %v\n", err)
+		os.Exit(1)
+	}
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+
 	serveMux := http.NewServeMux()
 
-	var cfg = &apiConfig{
-		fileserverHits: atomic.Int32{},
+	var cfg = &handler.ApiConfig{
+		FileserverHits: atomic.Int32{},
+		Queries:        database.New(db),
 	}
 
-	serveMux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
-	
-	serveMux.Handle("/admin/metrics/", http.HandlerFunc(cfg.metricsHandler))
-	serveMux.Handle("GET /api/healthz", http.HandlerFunc(healthzHandler))
+	serveMux.Handle("/app/", cfg.MiddlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
 
-	serveMux.Handle("GET /api/metrics", http.HandlerFunc(cfg.hitsHandler))
+	serveMux.Handle("/admin/metrics/", http.HandlerFunc(cfg.MetricsHandler))
+	serveMux.Handle("GET /api/healthz", http.HandlerFunc(handler.HealthzHandler))
 
-	serveMux.Handle("POST /admin/reset", http.HandlerFunc(cfg.resetHandler))
-	serveMux.Handle("POST /api/validate_chirp", http.HandlerFunc(cfg.handleValidateChirp))
+	serveMux.Handle("GET /api/metrics", http.HandlerFunc(cfg.HitsHandler))
 
-	server := http.Server {
+	serveMux.Handle("POST /admin/reset", http.HandlerFunc(cfg.ResetHandler))
+	serveMux.Handle("POST /api/validate_chirp", http.HandlerFunc(cfg.HandleValidateChirp))
+
+	server := http.Server{
 		Handler: serveMux,
 		Addr:    ":8080",
 	}
 
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	if err != nil {
 		fmt.Printf("Failed to start server: %v\n", err)
 		os.Exit(1)

5865a684085689931f44f1a69eb4b2cf0a9e7081
Bangalore Shankar Denanath, Chandan <49138440+chandanbsd@users.noreply.github.com>
2025-07-27 23:08:46 -0400

    Updates

--- main.go
+++ main.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	"net/http"
 	"os"
-	"slices"
-	"strings"
 	"sync/atomic"
 )
 
@@ -37,114 +35,62 @@ func (cfg *apiConfig) hitsHandler(resWriter http.ResponseWriter, req *http.Reque
 }
 
 func (cfg *apiConfig) resetHandler(resWriter http.ResponseWriter, req *http.Request) {
-	cfg.fileserverHits.Store(0)
+	cfg.fileserverHits.Store(0);
 }
 
 func (cfg *apiConfig) metricsHandler(resWriter http.ResponseWriter, req *http.Request) {
 	header := resWriter.Header()
-	header.Set("Content-Type", "text/html")
+	header.Set("Context-Type", "text/html")
 	resWriter.WriteHeader(200)
-	resWriter.Write(
-		[]byte(fmt.Sprintf(`<html>
+	resWriter.Write([]byte(fmt.Sprintf(`
+<html>
   <body>
     <h1>Welcome, Chirpy Admin</h1>
     <p>Chirpy has been visited %d times!</p>
   </body>
-</html>`, cfg.fileserverHits.Load())),
-	)
+</html>
+`, cfg.fileserverHits.Load())))
 }
 
-type ChirpPayload struct {
-	Body string `json:"body"`
-}
+func (cfg *apiConfig) handleValidateChirp(resWriter http.ResponseWriter, req *http.Request) {
 
-type ChirpInvalidDto struct {
-	Error string `json:"error"`
-}
-
-type ChirpCleanedDto struct {
-	CleanedBody string `json:"cleaned_body"`
-}
-
-func (cfg *apiConfig) validateChirpHandler(resWriter http.ResponseWriter, req *http.Request) {
-	profane_words_filter := []string{"kerfuffle", "sharbert", "fornax"}
-
-	decoder := json.NewDecoder(req.Body)
-	payload := ChirpPayload{}
-	header := resWriter.Header()
-	header.Set("Content-Type", "application/json")
-	err := decoder.Decode(&payload)
-
-	resBody := ChirpInvalidDto{
-		Error: "",
+	type payload struct {
+		body string `json:"body"`
 	}
+
+	type dto struct {
+		error string `json:"error"`
+	}
+
+	type dtoValid struct {
+		body string `json:"body"`
+	}
+
+	defer req.Body.Close()
+	decode := json.NewDecoder(req.Body)
+
+
+	var p payload
+
+	err := decode.Decode(&p)
 	if err != nil {
+		d := dtoValid {
+			body:  "This is an opinion I need to share with the world",
+		}	
+		dataBytes, _ := json.Marshal(d)
+
 		resWriter.WriteHeader(400)
-		resBody.Error = "Failed to decode the request body"
-
-		resBytes, err := json.Marshal(resBody)
-		if err != nil {
-			resWriter.Write(resBytes)
-			return
-		}
-	}
-
-	cleanedString := ""
-
-	profaneWordFound := false
-	totalWords := len(strings.Split(payload.Body, " "))
-	for i, word := range strings.Split(payload.Body, " ") {
-		if slices.Contains(profane_words_filter, strings.ToLower(word)) {
-			cleanedString += strings.Repeat("*", 4)
-			profaneWordFound = true
-		} else {
-			cleanedString += word
-		}
-		if totalWords != (i + 1) {
-			cleanedString += " "
-		}
-	}
-
-	if profaneWordFound == true {
-		dto := ChirpCleanedDto{
-			CleanedBody: cleanedString,
-		}
-
-		dtoBytes, err := json.Marshal(dto)
-		if err != nil {
-			panic("Failed to create response dto")
-		} else {
-			resWriter.WriteHeader(http.StatusOK)
-			resWriter.Write(dtoBytes)
-			return
-		}
-	}
-
-	if len(payload.Body) <= 140 {
-		resBody := ChirpCleanedDto{}
-		resBody.CleanedBody = payload.Body
-		resBodyBytes, err := json.Marshal(resBody)
-		if err != nil {
-			resBody := ChirpInvalidDto{}
-			resBody.Error = "Failed to decode the request body"
-			resBytes, _ := json.Marshal(resBody)
-			resWriter.WriteHeader(http.StatusBadRequest)
-			resWriter.Write(resBytes)
-			return
-		}
-		resWriter.WriteHeader(http.StatusOK)
-		resWriter.Write(resBodyBytes)
-		return
+		resWriter.Write(dataBytes)
 	} else {
-		resBody := ChirpInvalidDto{}
-		resBody.Error = "Chirp is too long"
-		resBytes, _ := json.Marshal(resBody)
-		resWriter.WriteHeader(http.StatusBadRequest)
-		resWriter.Write(resBytes)
-		return
+		d := dto {
+			error: "Something went wrong",
+		}	
+		dataBytes, _ := json.Marshal(d)
+		resWriter.WriteHeader(200)
+		resWriter.Write(dataBytes)
 	}
-
 }
+
 
 func main() {
 	serveMux := http.NewServeMux()
@@ -154,16 +100,16 @@ func main() {
 	}
 
 	serveMux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
+	
+	serveMux.Handle("/admin/metrics/", http.HandlerFunc(cfg.metricsHandler))
+	serveMux.Handle("GET /api/healthz", http.HandlerFunc(healthzHandler))
 
-	serveMux.HandleFunc("GET /admin/metrics/", http.HandlerFunc(cfg.metricsHandler))
+	serveMux.Handle("GET /api/metrics", http.HandlerFunc(cfg.hitsHandler))
 
-	serveMux.HandleFunc("GET /api/metrics", http.HandlerFunc(cfg.hitsHandler))
+	serveMux.Handle("POST /admin/reset", http.HandlerFunc(cfg.resetHandler))
+	serveMux.Handle("POST /api/validate_chirp", http.HandlerFunc(cfg.handleValidateChirp))
 
-	serveMux.HandleFunc("POST /api/reset", http.HandlerFunc(cfg.resetHandler))
-
-	serveMux.HandleFunc("POST /api/validate_chirp", http.HandlerFunc(cfg.validateChirpHandler))
-
-	server := http.Server{
+	server := http.Server {
 		Handler: serveMux,
 		Addr:    ":8080",
 	}

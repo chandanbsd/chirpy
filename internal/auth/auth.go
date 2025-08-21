16c3b08fc0837d50f24c29e3f257c915eebb0133
Bangalore Shankar Denanath, Chandan <49138440+chandanbsd@users.noreply.github.com>
2025-08-20 22:32:50 -0400

    Updates

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,42 +1,22 @@
 package auth
 
 import (
-	"encoding/json"
 	"errors"
 
 	"golang.org/x/crypto/bcrypt"
 )
 
 func HashPassword(password string) (string, error) {
-	passwordBytes, err := json.Marshal(password)
-
-	if err != nil {
-		return "", errors.New("failed to convert the password to bytes")
-	}
-
-	hashedBytes, err := bcrypt.GenerateFromPassword(passwordBytes, 1)
+	hashedBytes, err := bcrypt.GenerateFromPassword([]byte(password), 1)
 
 	if err != nil {
 		return "", errors.New("failed to generate a has from the password")
 	}
 
-	hashedPasswordAsString := string(hashedBytes)
-	return hashedPasswordAsString, err
+	return string(hashedBytes), err
 }
 
 func CheckPasswordHash(password string, hash string) error {
-
-	passwordBytes, err := json.Marshal(password)
-
-	if err != nil {
-		return errors.New("Failed to transform the password provided")
-	}
-
-	hashBytes, err := json.Marshal(hash)
-	if err != nil {
-		return errors.New("Please contact the administrator to reset your password")
-	}
-
-	err = bcrypt.CompareHashAndPassword(passwordBytes, hashBytes)
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err
 }

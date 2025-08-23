7594fcbfd4a7946bb8397f53e0f93ed4cc298df9
Bangalore Shankar Denanath, Chandan <49138440+chandanbsd@users.noreply.github.com>
2025-08-23 10:02:33 -0400

    Addded helper function to generate auth tokens

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -2,7 +2,10 @@ package auth
 
 import (
 	"errors"
+	"time"
 
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -19,4 +22,23 @@ func HashPassword(password string) (string, error) {
 func CheckPasswordHash(password string, hash string) error {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	
+	claim := jwt.RegisteredClaims {
+		Issuer: "chirpy",
+		IssuedAt: jwt.NewNumericDate(time.Now()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(expiresIn)),
+		Subject: userID.String(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claim)
+
+	signedString, err := token.SignedString(tokenSecret)
+	if err != nil {
+		return "", errors.New("Failed to generate the auth token")
+	}
+
+	return signedString, nil
 }
